d6143e425abc0296adb2f8dcdd027df74cd7d2c2
nessian-git <24859802+nessian-git@users.noreply.github.com>
2023-11-11 12:30:52 -0800

    Basic outline page shows nested list of all chapters with links

--- outline.py
+++ outline.py
@@ -13,6 +13,41 @@ class Outline:
 
     def createHTML(self):
         path = self.root.dir
+
+        #Base HTML
+        with open('default.html', 'r') as f:
+            html = BeautifulSoup(f.read(), 'html.parser')
+        stylesheet = [link for link in html.findAll("link") if "style.css" in link.get("href", [])][0]
+        stylesheet['href'] = "chapters/style.css"
+        body = html.find("body")
+
+        header = html.new_tag('h1')
+        header.string = "Outline"
+
+        body.append(header)
+
+        soup = BeautifulSoup('', 'html.parser')
+        list = soup.new_tag('ul')
+        list.append(self.traverse(self.root, soup))
+        body.append(list)
+
         with open(f'{path}/outline.html', 'w', encoding="utf-8") as file:
             # Further file processing goes here
-            file.write("<!DOCTYPE html>\n")
+            file.write(str(html))
+
+    def traverse(self, page: Page, soup: BeautifulSoup):
+        li_tag = soup.new_tag("li")
+        if page.root == page:
+            a_tag = soup.new_tag("a", href=page.filename)
+        else:
+            a_tag = soup.new_tag("a", href=f"chapters/{page.filename}") 
+        a_tag.string = page.name
+        li_tag.append(a_tag)
+
+        if page.children:
+            ul_tag = soup.new_tag("ul")
+            for child in page.children:
+                ul_tag.append(self.traverse(child, soup))
+            li_tag.append(ul_tag)
+        return li_tag
+
