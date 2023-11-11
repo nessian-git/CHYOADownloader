506801e0105415a70dd9bf89e33141faafbd42b8
nessian-git <24859802+nessian-git@users.noreply.github.com>
2023-11-11 14:30:39 -0800

    Removed redundant command line arg "link" as "links" has more functionality

--- main.py
+++ main.py
@@ -18,8 +18,7 @@ class recursionlimit:
 
 parser = argparse.ArgumentParser()
 
-parser.add_argument('--link', help="URL of CHYOA start", type=str)
-parser.add_argument('--links', help="Multiple URLS", type=str)
+parser.add_argument('--links', help="Comma-separated list of CHYOA URLS. Can provide just one URL without comma for single download", type=str)
 parser.add_argument('--images', help="Download images", type=bool, default=True)
 parser.add_argument('--directory','-d', help="Directory to store downloaded files", default=os.getcwd(), type=str)
 parser.add_argument('--download_delay','-e', help="Wait this many seconds between download requests to avoid overloading the server", default=0, type=int)
@@ -32,24 +31,16 @@ args = vars(parser.parse_args())
 
 with recursionlimit(30000):
     print(sys.getrecursionlimit())
-    if args['links']:
-        links = []
-        for i in args['links'].split(","):
-            print("Collecting Links From " + i)
-            page = Page(i, "", args['directory'], args['images'], download_delay=max(0, args['download_delay']))
-            print("Links Collected")
-            print("Building HTML Files")
-            page.createHTML()
-            print("Download Complete")
-            print("Creating outline")
-            outline = Outline(page)
-            outline.createHTML()
-            print("Outline Created")
-        print("All Files Downloaded")
-    else:
-        print("Collecting Links")
-        page = Page(args['link'], "", args['directory'], args['images'], download_delay=args['download_delay'])
+    links = []
+    for i in args['links'].split(","):
+        print("Collecting Links From " + i)
+        page = Page(i, "", args['directory'], args['images'], download_delay=max(0, args['download_delay']))
         print("Links Collected")
         print("Building HTML Files")
         page.createHTML()
         print("Download Complete")
+        print("Creating outline")
+        outline = Outline(page)
+        outline.createHTML()
+        print("Outline Created")
+    print("All Files Downloaded")
